d71ef8a7370e23d2154c81b6447e748500e6799f
lovesherry <1209478498@qq.com>
2018-11-29 20:28:04 +0800

    finish 283-move-zeroes

---
+++ src/283-move-zeroes.js
@@ -0,0 +1,15 @@
+var moveZeroes = function(nums) {
+    var i=0,j=0;
+    for(;j<nums.length;j++){
+        if(nums[j]!==0){
+            if(i!==j){
+                nums[i] = nums[j]; 
+            }       
+            i++;            
+        }
+    }
+    for(var k=nums.length-1;k>=i;k--){
+        nums[k] = 0;
+    }
+    return nums;
+};
